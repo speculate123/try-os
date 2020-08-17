2af97f1a472b91c69c8a45fccc2b1e3cad46e0be
speculate123 <66710736+speculate123@users.noreply.github.com>
2020-08-17 02:12:27 -0400

    Update write_allfilepath_to_txt.py

--- write_allfilepath_to_txt.py
+++ write_allfilepath_to_txt.py
@@ -1,8 +1,19 @@
 import os
 import glob
+import random
 
-fishdir = 'home/abc/darknet_fish/fish_data'
-filename = glob.glob(os.path.join(fishdir, 'good_test/*.jpg'))
-with open('test.txt', 'w') as f:
-  for i in range(len(filename)):
+datadir = '/mlsteam/lab/darknet/data_roaddamage/'
+
+filename = glob.glob(os.path.join(datadir, 'Adachi/*.jpg'))
+
+random.shuffle(filename)
+
+split = int(len(filename)*0.9)
+
+with open('train.txt', 'w') as f:
+  for i in range(split):
     f.write(filename[i] + '\n')
+    
+with open('test.txt', 'w') as f:
+  for i in range(len(filename)-split):
+    f.write(filename[i+split] + '\n')
